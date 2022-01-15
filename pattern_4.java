67e904d61b9468837b50c4c9b4061d1c27ef2066
NaveenSiva72 <naveensiva72@gmail.com>
2022-01-15 18:46:08 +0530

    day 4

---
+++ pattern_4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class pattern_4
+{
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            int number = 1;
+            System.out.printf("%" + (n - i) * 2 + "s", "");
+            for (int j = 0; j <= i; j++) {
+                System.out.printf("%4d", number);
+                number = number * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+
+    }
+
+}
