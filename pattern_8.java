0a0f74ace00ca494b0a12e3e01b4fe1095dc67bd
NaveenSiva72 <naveensiva72@gmail.com>
2022-01-19 21:40:52 +0530

    day 8

---
+++ pattern_8.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class pattern_8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = input.nextInt();
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                if(j%2 == 0)
+                {
+                    System.out.print(0);
+                }
+                else
+                {
+                    System.out.print(1);
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+}
