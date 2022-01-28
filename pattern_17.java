4cf5166b2026f9e322668a4a71595a623a80ece3
NaveenSiva72 <naveensiva72@gmail.com>
2022-01-28 20:25:43 +0530

    day 17

---
+++ pattern_17.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class pattern_17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the no.of rows: ");
+        int rows = scanner.nextInt();
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        for (int i = rows; i >= 1; i--)
+        {
+            for (int j = 1; j < i; j++)
+            {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
